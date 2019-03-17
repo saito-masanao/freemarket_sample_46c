51cda62d7f0db7f872fac7c53b53de0767d687bb
tomoakimiura <tomomiura0026@gmail.com>
2019-03-17 18:20:58 +0900

    細かいhaml,scssの修正

--- db/schema.rb
+++ db/schema.rb
@@ -9,10 +9,14 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+<<<<<<< Updated upstream
 
 
 ActiveRecord::Schema.define(version: 2019_03_17_043359) do
 
+=======
+ActiveRecord::Schema.define(version: 2019_03_14_095802) do
+>>>>>>> Stashed changes
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
