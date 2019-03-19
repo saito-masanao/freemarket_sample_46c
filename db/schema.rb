2071ff76cb583927cbd3536f7e138c20e09b373c
saito-masanao <saitoumasanao@saitoumasanaonoMacBook-Pro.local>
2019-03-19 16:35:57 +0900

    htmlファイルの準備

--- db/schema.rb
+++ db/schema.rb
@@ -9,14 +9,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-<<<<<<< Updated upstream
-
 
 ActiveRecord::Schema.define(version: 2019_03_17_043359) do
-
-=======
-ActiveRecord::Schema.define(version: 2019_03_14_095802) do
->>>>>>> Stashed changes
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,14 +20,12 @@ ActiveRecord::Schema.define(version: 2019_03_14_095802) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
-    t.string "nickname", null: false
+    t.integer "birth_day", null: false
     t.string "first_name"
     t.string "last_name"
     t.string "first_name_kana"
     t.string "last_name_kana"
     t.date "birthday"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
