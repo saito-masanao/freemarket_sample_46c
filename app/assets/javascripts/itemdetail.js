7fcc98597f40484f165285b541ba9da496487ecb
saito-masanao <saitoumasanao@saitoumasanaonoMacBook-Pro.local>
2019-03-21 21:11:09 +0900

    correct

--- app/assets/javascripts/itemdetail.js
+++ app/assets/javascripts/itemdetail.js
@@ -1,12 +1,22 @@
-var slider = function() {
-  var $slider = $('.slider');
+$(function() {
+  var $slider = $('.is-slider');
   if ($slider.length < 1) { return; }
 
-  var $thumbs = $('.thumbnails_item');
+  var $thumbs = $('.is-thumbnails__item');
 
   $slider.slick({
     arrows: false
   });
-};
 
-slider();
+  $thumbs.on('click', function(e){
+    e.preventDefault();
+    var index = $thumbs.index(this);
+    $slider.slick('slickGoTo', index, true);
+  });
+
+  $slider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    var currentClass = 'current';
+    $thumbs.removeClass(currentClass)
+      .eq(nextSlide).addClass(currentClass);
+  });
+});
--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 2019_03_17_084907) do
 
   create_table "social_profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
