1ab0f3b681a75a599f15113e4954e20b7b9f5a0c
YKKISKW <yukako1160101@gmail.com>
2019-03-21 14:07:42 +0900

    ✍️ユーザー新規登録のview修正
    link_to image_tagを使用したマークアップに修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     omniauth_callbacks: "omniauth_callbacks",
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'users#sign_up'
   get 'users/new'
   post 'users/create'
