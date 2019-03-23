71d0d3b7d3c4375a5b494a243c9eca0099077f3c
YKKISKW <yukako1160101@gmail.com>
2019-03-23 15:22:10 +0900

    ✍️item関連テーブルとモデルの作成
    item,category,brand,imageモデルの生成
    enum,seed.rbを使用したデータの作成

---
+++ app/models/image.rb
@@ -0,0 +1,3 @@
+class Image < ApplicationRecord
+  belomgs_to :image
+end
---
+++ app/models/item.rb
@@ -0,0 +1,30 @@
+class Item < ApplicationRecord
+  belongs_to_active_hash :prefecture
+  belongs_to :category
+  belongs_to :brand
+  has_many :images, dependent: :destory
+
+  enum status: {
+                  very_good: 0,
+                  good:      1,
+                  normal:    2,
+                  bad:       3,
+                  so_bad:    4,
+                  very_bad:  5
+  }
+  enum derivery_fee: {Included:0,Exculude:1}
+  enum derivery_date: { OneToTwo: 0, TwoToThree: 1,FourToSeven: 2}
+  enum delivery_method: {
+                          undecided:           0,
+                          easy_mercari_mail:   1,
+                          yu_yu_mercari_mail:  2,
+                          large_mericari_mail: 3,
+                          postal_mail:         4,
+                          letter_pack:         5,
+                          regular_mail:        6,
+                          kuroneko_yamato:     7,
+                          yu_pack:             8,
+                          click_post:          9,
+                          yu_packet:           10,
+}
+end
