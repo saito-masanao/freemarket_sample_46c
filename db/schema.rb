fb64f014bf442b770e7db309d0bcfab6f5f51e42
tomoakimiura <tomomiura0026@gmail.com>
2019-03-23 16:23:01 +0900

    seedの取得

--- db/schema.rb
+++ db/schema.rb
@@ -79,7 +79,7 @@ ActiveRecord::Schema.define(version: 2019_03_23_063528) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "nickname", null: false
+    t.integer "birth_day", null: false
     t.string "first_name"
     t.string "last_name"
     t.string "first_name_kana"
