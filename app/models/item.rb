758a5fc47e06f496870abcb2ee62c18dcfcde577
YKKISKW <yukako1160101@gmail.com>
2019-03-23 15:52:38 +0900

    ✍️sizeカラムと値の追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -25,6 +25,18 @@ class Item < ApplicationRecord
                           kuroneko_yamato:     7,
                           yu_pack:             8,
                           click_post:          9,
-                          yu_packet:           10,
+                          yu_packet:           10
 }
+  enum size: {
+                underXXS: 0,
+                XS: 1,
+                S: 2,
+                M: 3,
+                L: 4,
+                XL: 5,
+                two_XL: 6,
+                three_XL: 7,
+                four_XL: 8,
+                FREE_SIZE: 9
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_23_061333) do
+ActiveRecord::Schema.define(version: 2019_03_23_063528) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_03_23_061333) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "size"
     t.index ["brand_id"], name: "index_items_on_brand_id"
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["user_id"], name: "index_items_on_user_id"
