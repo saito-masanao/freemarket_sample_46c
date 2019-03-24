2ee6e51620dd52095f5dc5c6c62fbecdfe7f53e6
tomoakimiura <tomomiura0026@gmail.com>
2019-03-24 22:46:07 +0900

    部分テンプレートの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
+
+  def top
+    @items = Item.all.order("created_at DESC").limit(4)
+    @nikes = Item.where(brands: 2).order("created_at DESC").limit(4)
+  end
+
   def new
   end
 
