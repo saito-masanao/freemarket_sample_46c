7699187f5e2556712ebf84d4b3e2d978dc0c9a15
tomoakimiura <tomomiura0026@gmail.com>
2019-03-25 18:59:11 +0900

    vuittonの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,8 @@ class ItemsController < ApplicationController
 
   def top
     @nikes = Item.where(brand_id: 2).order("created_at DESC").limit(4)
-    @supremes =Item.where(brand_id: 4).order("created_at DESC").limit(4)
+    @supremes = Item.where(brand_id: 4).order("created_at DESC").limit(4)
+    @vuittons = Item.where(brand_id: 3).order("created_at DESC").limit(4)
   end
 
   def new
