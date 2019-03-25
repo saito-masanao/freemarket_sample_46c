5832c2a9687d69b2b75c84ae58af4da01728b24c
tomoakimiura <tomomiura0026@gmail.com>
2019-03-25 16:13:03 +0900

    nikeのrender

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,7 @@
 class ItemsController < ApplicationController
 
   def top
-    @items = Item.all.order("created_at DESC").limit(4)
-    @nikes = Item.where(brands: 2).order("created_at DESC").limit(4)
+    @nikes = Item.where(brand_id: 2).order("created_at DESC").limit(4)
   end
 
   def new
