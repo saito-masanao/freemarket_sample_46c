c324fbdce81f8b58bd8097a3b503394fb02b88f7
tomoakimiura <tomomiura0026@gmail.com>
2019-03-25 19:15:03 +0900

    chanelの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,7 @@ class ItemsController < ApplicationController
     @nikes = Item.where(brand_id: 2).order("created_at DESC").limit(4)
     @supremes = Item.where(brand_id: 4).order("created_at DESC").limit(4)
     @vuittons = Item.where(brand_id: 3).order("created_at DESC").limit(4)
+    @chanels = Item.where(brand_id: 1).order("created_at DESC").limit(4)
   end
 
   def new
