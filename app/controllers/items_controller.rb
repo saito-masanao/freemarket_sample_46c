5c9f346b80d78956d1e909921992ed09d5313a89
tomoakimiura <tomomiura0026@gmail.com>
2019-03-25 18:24:03 +0900

    supremeの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
 
   def top
     @nikes = Item.where(brand_id: 2).order("created_at DESC").limit(4)
+    @supremes =Item.where(brand_id: 4).order("created_at DESC").limit(4)
   end
 
   def new
