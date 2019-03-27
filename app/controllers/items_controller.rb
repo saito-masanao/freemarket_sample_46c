21ff33dc03d9e3fbeb9c497dd3208586d94c23a6
tomoakimiura <tomomiura0026@gmail.com>
2019-03-27 14:09:57 +0900

    cosmeticsの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,9 @@ class ItemsController < ApplicationController
 
   def top
     @ladies = Item.where(category_id: 1).order("created_at DESC").limit(4)
+    @mens = Item.where(category_id: 1).order("created_at DESC").limit(4)
+    @kids = Item.where(category_id: 3).order("created_at DESC").limit(4)
+    @cosmetics = Item.where(category_id: 7).order("created_at DESC").limit(4)
     @nikes = Item.where(brand_id: 2).order("created_at DESC").limit(4)
     @supremes = Item.where(brand_id: 4).order("created_at DESC").limit(4)
     @vuittons = Item.where(brand_id: 3).order("created_at DESC").limit(4)
