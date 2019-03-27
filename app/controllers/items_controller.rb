700fcc39e573c75139037b1f84ebf8aa9c27ebc3
tomoakimiura <tomomiura0026@gmail.com>
2019-03-27 13:43:13 +0900

    ladiesの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
   def top
+    @ladies = Item.where(category_id: 1).order("created_at DESC").limit(4)
     @nikes = Item.where(brand_id: 2).order("created_at DESC").limit(4)
     @supremes = Item.where(brand_id: 4).order("created_at DESC").limit(4)
     @vuittons = Item.where(brand_id: 3).order("created_at DESC").limit(4)
