a82512935a6a4ea6a729cbe0efb3dd755c4ad962
tomoakimiura <tomomiura0026@gmail.com>
2019-03-27 14:32:28 +0900

    topの完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,10 +5,10 @@ class ItemsController < ApplicationController
     @mens = Item.where(category_id: 1).order("created_at DESC").limit(4)
     @kids = Item.where(category_id: 3).order("created_at DESC").limit(4)
     @cosmetics = Item.where(category_id: 7).order("created_at DESC").limit(4)
-    @nikes = Item.where(brand_id: 2).order("created_at DESC").limit(4)
-    @supremes = Item.where(brand_id: 4).order("created_at DESC").limit(4)
-    @vuittons = Item.where(brand_id: 3).order("created_at DESC").limit(4)
     @chanels = Item.where(brand_id: 1).order("created_at DESC").limit(4)
+    @vuittons = Item.where(brand_id: 3).order("created_at DESC").limit(4)
+    @supremes = Item.where(brand_id: 4).order("created_at DESC").limit(4)
+    @nikes = Item.where(brand_id: 2).order("created_at DESC").limit(4)
   end
 
   def new
