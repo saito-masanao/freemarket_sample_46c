0b1406e70b657823bc19ac8608a91d95300d3475
YKKISKW <yukako1160101@gmail.com>
2019-03-29 11:21:42 +0900

    レビュー前の諸々の修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items,only: [:new, :create]
-  root 'users#sign_up'
+  root 'users#top'
   get 'users/new'
   post 'users/create'
   get 'top', to: 'users#top'
