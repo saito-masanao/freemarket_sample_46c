470c6f1dabbf9faafc0f42bec1d7f2ee649de31d
98hira <flattail2018@gmail.com>
2019-03-29 16:35:03 +0900

    💳カード情報へアクセスするためのcard_idをDBへ登録する処理を追加
    [要検討]
    全てコントローラで処理してるがこれは正しいの❓
    エラー処理は書いてないので、書かないといけない(´・ω・｀)
    
    [time]2019/03/29-16:37

--- app/controllers/credit_card_controller.rb
+++ app/controllers/credit_card_controller.rb
@@ -1,4 +1,5 @@
 class CreditCardController < ApplicationController
+  # before_action :get_token, only: [:create]
 
   # 支払い方法確認画面
   def index
@@ -15,11 +16,30 @@ class CreditCardController < ApplicationController
   # カード情報登録処理
   def create
     # binding.pry #[@kari]
+    _token = get_token_param()
+
+    #APIキーの設定
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+
+    # トークン情報を元に顧客を作成
+    _customer = Payjp::Customer.create(description: 'test')
+
+    #顧客のカードを作成
+    _customer_obj = Payjp::Customer.retrieve(_customer[:id])
+    _customer_card = _customer_obj.cards.create(card: _token)
+
+    # 作成した顧客をDBへ保存
+    new_record = {
+      user_id: 1,
+      card_id: _customer_card[:id]
+    }
+    credit_record = Credit.new(new_record)
+    credit_record.save
   end
 
   # カード情報削除画面
   def destroy
-    # binding.pry #[@kari]
+    binding.pry #[@kari]
   end
 
   # [@ToDo]今のところ使う予定なし
@@ -30,5 +50,9 @@ class CreditCardController < ApplicationController
   def update
   end
 
+  private
+  def get_token_param
+    return params.require(:card_token)
+  end
 
 end
