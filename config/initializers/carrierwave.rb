a2f3bcf63fd87142fdc9496a28f9fd40ec1d352b
YKKISKW <yukako1160101@gmail.com>
2019-03-29 17:11:08 +0900

    carrierwave.rbを元に戻した

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,13 +5,10 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
-  SECRETS_CONFIG = YAML.load_file("#{Rails.root}/config/secrets.yml")[Rails.env].symbolize_keys!
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: SECRETS_CONFIG[:aws_access_key_id],
-    # Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: SECRETS_CONFIG[:aws_secret_access_key],
-    # Rails.application.secrets.aws_secret_access_key,
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
   }
 
