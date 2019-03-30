82d4b5c506d6e1241d0facba1b9b263f96b498e3
hiroaki122 <jiro.h.v.122@gmail.com>
2019-03-30 18:08:08 +0900

    [WIP]プルダウンメニュー実装２

--- app/assets/javascripts/top-page_category.js
+++ app/assets/javascripts/top-page_category.js
@@ -8,4 +8,22 @@ $(function () {
       $(this).children('ul').hide();
     }
   );
+  $('.top-main-header-nav-parent').hover(
+    function () {
+      $(this).children('ul').show();
+    },
+    function () {
+      $(this).children('ul').hide();
+    }
+  );
+  $('.top-main-header-nav-child').hover(
+    function () {
+      $(this).children('ul').show();
+    },
+    function () {
+      $(this).children('ul').hide();
+    }
+  );
+
+
 });
