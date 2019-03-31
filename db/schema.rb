b3ec518f45d2d5166d9d42fd63f0c18e2dd2e9b3
saito-masanao <saitoumasanao@saitoumasanaonoMacBook-Pro.local>
2019-03-31 18:40:40 +0900

    Correct schema_file

--- db/schema.rb
+++ db/schema.rb
@@ -98,12 +98,12 @@ ActiveRecord::Schema.define(version: 2019_03_27_063517) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "birth_day", null: false
     t.string "first_name"
     t.string "last_name"
     t.string "first_name_kana"
     t.string "last_name_kana"
     t.date "birthday"
-    t.string "nickname", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
