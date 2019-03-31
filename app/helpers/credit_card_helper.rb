71aaa38d7ab865a0260c1139c3674c134bc5fb75
98hira <flattail2018@gmail.com>
2019-03-31 15:09:18 +0900

    💳ブランドロゴ表示処理のヘルパー化
    ヘルパー化した理由：
    ユーザーが登録したクレジットカードのブランドロゴを表示する処理で流用できるため。
    
    [time]2019/03/31-15:10

--- app/helpers/credit_card_helper.rb
+++ app/helpers/credit_card_helper.rb
@@ -1,2 +1,14 @@
 module CreditCardHelper
+  def get_credit_log(bland)
+    bland_dic = {
+      visa:      "//www-mercari-jp.akamaized.net/assets/img/card/visa.svg?4136293187",
+      master:    "//www-mercari-jp.akamaized.net/assets/img/card/master-card.svg?4136293187",
+      saison:    "//www-mercari-jp.akamaized.net/assets/img/card/saison-card.svg?4136293187",
+      jcb:       "//www-mercari-jp.akamaized.net/assets/img/card/jcb.svg?4136293187",
+      american:  "//www-mercari-jp.akamaized.net/assets/img/card/american_express.svg?4136293187",
+      dinners:   "//www-mercari-jp.akamaized.net/assets/img/card/dinersclub.svg?4136293187",
+      discover:  "//www-mercari-jp.akamaized.net/assets/img/card/discover.svg?4136293187",
+    }
+    return image_tag(bland_dic[bland], class:bland.to_s)
+  end
 end
