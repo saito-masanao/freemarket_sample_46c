826c34b8f9a88db098f9faf49a3de06e8070e4fd
98hira <flattail2018@gmail.com>
2019-04-01 13:42:24 +0900

    💳不要な自動生成ファイルの削除
    [time]2019/04/01-13:42

--- spec/controllers/credit_card_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe CreditCardController, type: :controller do
-
-end
--- spec/factories/credits.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :credit do
-    user_id { 1 }
-    token { 1 }
-  end
-end
