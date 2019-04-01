174d5049f8b1aa48ae6b63fa0243014999939ed3
hiroaki122 <jiro.h.v.122@gmail.com>
2019-04-01 19:51:20 +0900

    マイグレーションファイル整理

--- db/migrate/20190327123801_add_category_id_to_middle_categories.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryIdToMiddleCategories < ActiveRecord::Migration[5.2]
-  def change
-    add_column :middle_categories, :category_id, :integer
-  end
-end
--- db/migrate/20190327125305_add_middle_category_id_to_lower_categories.rb
+++
@@ -1,5 +0,0 @@
-class AddMiddleCategoryIdToLowerCategories < ActiveRecord::Migration[5.2]
-  def change
-    add_column :lower_categories, :middle_category_id, :integer
-  end
-end
