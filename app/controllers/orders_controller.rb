6549f736f9e03db69f0a340d498829d1cf9b049f
98hira <flattail2018@gmail.com>
2019-04-02 21:54:20 +0900

    👛商品購入ときのサーバーサイドの処理を実装
    [time]2019/04/02-21:55

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,15 +9,44 @@ class OrdersController < ApplicationController
 
   #商品購入処理
   def create
+    _success_flag = true
+    _param = get_order_param()
+
+    begin
+      ActiveRecord::Base.transaction do
+        #アイテムステータスの更新
+        _item = Item.find(_param[:item_id])
+        _item.update(item_status: get_item_status())
+        #オーダーテーブルの生成
+        _order = Order.new(_param)
+        _order.save
+      end
+    rescue => e
+      _success_flag = false
+    end
+
+    @MESSAGE = (_success_flag) ? "成功": "失敗"
   end
 
   private
 
+  def get_order_param()
+    return {
+      user_id: params[:user_id].to_i,
+      item_id: params[:item_id].to_i,
+    }
+  end
+
+  # [@ToDo] itemテーブルのitem_statusカラムを商品購入状態に変更する仮の値を取得する。
+  def get_item_status()
+    return 1
+  end
+
+  # [@ToDo]Userテーブルが完璧出ないため仮のダミーデータを生成している
   def get_user_info()
     _w = current_user.slice(:id, :last_name, :first_name, :nickname)
     _ret = {}
 
-    # [@ToDo]Userテーブルが完璧出ないため仮のダミーデータを生成している
     if _w[:last_name] && _w[:first_name]
       _ret[:name] = "#{_w[:last_name]} #{_w[:first_name]}"
     elsif _w[:nickname]
@@ -31,4 +60,5 @@ class OrdersController < ApplicationController
 
     return _ret
   end
+
 end
