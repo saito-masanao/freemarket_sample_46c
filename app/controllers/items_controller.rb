5fa332ee185d4807120be6a563f36f068aab1ba1
kenta-hiraoka <k.h.kentahiraoka@gmail.com>
2019-04-02 15:52:36 +0900

    Item-search1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,16 +6,24 @@ class ItemsController < ApplicationController
   end
 
   def search
-    if params[:item].blank?
+    result = []
+    if params[:keyword].blank?
       redirect_to root_path
     else
-      split_keyword = params[:keyword].split(/[[:blank:]]+/) #空白で分割
-      @items = []
-      split_keyword.each do |keyword| #分割したキーワード毎に検索
-        next if keyword == "" #先頭に空白があってもその後のキーワードで検索
-        @items = Item.where("name LIKE(?) OR description LIKE(?)", "%#{keyword}%","%#{keyword}%").page(params[:page]).per(48)
-        @all_items = Item.order("id DESC")
+      split_keyword = params[:keyword].split(/[[:blank:]]+/)
+      split_keyword.each_with_index do |keyword, index|
+        if index == 0
+          result[index] = Item.where("name LIKE(?) OR description LIKE(?)", "%#{keyword}%","%#{keyword}%")
+        else
+          result[index] = result[index-1].where("name LIKE(?) OR description LIKE(?)", "%#{keyword}%","%#{keyword}%")
+        end
       end
+        if result[split_keyword.length-1] != []
+          @items = result[split_keyword.length-1].page(params[:page]).per(48)
+        else
+          @items = Item.limit(24).order("id DESC").page(params[:page]).per(48)
+          @error = "該当する商品が見当たりません。商品は毎日増えていますので、これからの出品に期待してください。"
+        end
     end
   end
 end
