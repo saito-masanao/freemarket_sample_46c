b122b08aef5a134298dd810495d3dddeca9ef13a
YKKISKW <47511324+YKKISKW@users.noreply.github.com>
2019-04-02 16:18:08 +0900

    不要なマイグレーションファイルのため

--- db/migrate/20190327122911_add_name_to_middle_categories.rb
+++
@@ -1,6 +0,0 @@
-class AddNameToMiddleCategories < ActiveRecord::Migration[5.2]
-  def change
-    add_column :middle_categories, :name, :string
-    add_column :middle_categories, :category_id, :integer
-  end
-end
