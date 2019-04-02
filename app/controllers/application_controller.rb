abc228b6c866bd9099d25cabf7b0d9ce7bec6f83
tomoakimiura <tomomiura0026@gmail.com>
2019-04-02 17:37:00 +0900

    パラメーターの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name,:last_name,:first_name_kana,:last_name_kana,:birth_day])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name,:last_name,:first_name_kana,:last_name_kana,:year, :month, :day])
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_27_063517) do
+ActiveRecord::Schema.define(version: 2019_04_02_072440) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -98,12 +98,13 @@ ActiveRecord::Schema.define(version: 2019_03_27_063517) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.string "first_name_kana"
+    t.string "first_name", default: ""
+    t.string "last_name", default: ""
+    t.string "first_name_kana", default: ""
     t.string "last_name_kana"
     t.date "birthday"
     t.string "nickname", null: false
+    t.integer "year"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
